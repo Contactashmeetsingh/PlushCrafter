7fbc1b785d6b0a67cbe4d59625c4c891b523f04d
Ashmeet Singh <ashmeetsingh@ucmerced.edu>
2025-05-19 22:34:32 -0700

    Initial Commit

---
+++ Application.cpp
@@ -0,0 +1,12 @@
+#include "Application.h"
+#include <iostream>  // You need this for cout
+
+
+using namespace std;
+
+Application::Application(){
+    cout<<"hello world"<<endl;
+}
+void Application::run(){
+        cout << "Running PlushCrafter..." << endl;
+}
